288179e5fd6ec2ad2dd0d2ae039e70d036e18a07
Cường Bùi <cuong.bpt82@gmail.com>
2019-12-04 00:23:39 +0700

    Update user route with controller and service. Added ErrorHandler, AsyncMiddleware.

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const config = require("../config");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -16,7 +17,7 @@ const userSchema = mongoose.Schema({
         lowercase: true,
         validate: value => {
             if (!validator.isEmail(value)) {
-                throw new Error({error: 'Invalid Email address'})
+                throw new Error({ error: 'Invalid Email address' })
             }
         }
     },
@@ -24,6 +25,25 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         minLength: 7
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    },
+    tel: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    salary: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    photoLink: {
+        type: String,
+        trim: true,
+        required: false
     },
     tokens: [{
         token: {
@@ -43,40 +63,40 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre('save', async function (next) {
     // Hash the password before saving the user model
-    const user = this
+    const user = this;
     if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
+        user.password = await bcrypt.hash(user.password, 8);
     }
-    next()
+    next();
 })
 
-userSchema.methods.generateAuthToken = async function() {
+userSchema.methods.generateAuthToken = async function () {
     // Generate an auth token for the user
-    const user = this
-    const jwt_key = process.env.JWT_KEY || "WinterIsComingGOT2019"
-    const token = jwt.sign({_id: user._id}, jwt_key)
-    var tokens = user.tokens
+    const user = this;
+    const jwt_key = config.jwtKey;
+    const token = jwt.sign({ _id: user._id }, jwt_key);
+    var tokens = user.tokens;
     if (tokens.length > 9) {
-        tokens = tokens.slice(tokens.length - 9)
+        tokens = tokens.slice(tokens.length - 9);
     }
-    user.tokens = tokens.concat({token})
-    await user.save()
-    return token
+    user.tokens = tokens.concat({ token });
+    await user.save();
+    return token;
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
     // Search for a user by email and password.
-    const user = await User.findOne({ email} )
+    const user = await User.findOne({ email });
     if (!user) {
-        throw new Error({ error: 'Invalid login credentials' })
+        throw new Error({ error: 'Invalid login credentials' });
     }
-    const isPasswordMatch = await bcrypt.compare(password, user.password)
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
     if (!isPasswordMatch) {
-        throw new Error({ error: 'Invalid login credentials' })
+        throw new Error({ error: 'Invalid login credentials' });
     }
-    return user
+    return user;
 }
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-module.exports = User
+module.exports = User;
